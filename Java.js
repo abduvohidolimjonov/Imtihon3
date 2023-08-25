f1130e6d53ec335d3a389bea739c3a4827f9e32b
abduvohidolimjonov <abduabdulhamid@gmail.com>
2023-08-25 12:41:56 +0500

    imtihon

---
+++ Java.js
@@ -0,0 +1,59 @@
+//1 - savol //Object kalit va qiymat juftligidir bu bir katta to'plamdir. Kalitning qiymati oddiygina string, raqam, boshqa object va xatto funksiya ham bo'lishi mumkin.
+// //Bu Qiymati string bo'lgan Object
+// const name = 'Abduvohid';
+
+// //Bu Qiymatili bo'lgan Object
+// let number = 2009;
+
+
+// // Bu Object bo'lgan Object
+// const car = {
+//     make: 'Tesla',
+//     model: 'Model X',
+//     year: 2020
+// }
+
+// // Qiymati funksiya bo'lgan object
+// const greeting = function(){
+//    return 'Assalamu alaykum!'
+// }
+
+//
+
+// 2 -savol// 
+// let a="1304"
+//  let d=parseInt(a/1000)%10;
+//  let s=parseInt(a/100)%10;
+//  let f=parseInt(a/10)%10;
+//  let r=parseInt(a%10)
+//  console.log(r,s,f,d)
+
+//tugadi//
+
+// 3 -savol /case3/
+
+// let: a=12;
+// // switch (a) {
+// //     case 1,2,12:
+// //         console.log("Qish")
+// //         break
+// //         case 3,4,5:
+// //             console.log("Bahor")
+// //         break;
+// //         case 6,7,8:
+// //             console.log("Yoz")
+// //         break;
+// //         case 9,10,11:
+// //             console.log("Kuz")
+// //         break;
+// //     } 
+//    //Tugadi;
+
+    //4-savol
+
+//     let n =80;
+// let tenthDigit = Math.floor(n / 10) % 10;
+// console.log("O'nliklar xonasidagi raqam: " + tenthDigit);
+     
+
+//Funksiyalar Javascriptning eng asosiy tushunchalaridir biri hisoblanadi. Dasturlashni endi o'raganayotganlar uchun Funksiyalar biroz qiyindek tuyulishi mumkin, sababi funksiyalar turli ko'rinishlarda keladi.
